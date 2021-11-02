8aad4ef0d8fc1ab16e9d7cfbeb10b121029b7854
Jose Dominic <josudominic007@gmail.com>
2021-11-02 14:23:28 +0530

    added login component in auth module, added lazy loading for users module

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
+  exports: [
+    LoginComponent
+  ]
+  ,
   imports: [
     CommonModule
   ]
--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UsersRoutingModule } from './users-routing.module';
+import { UsersComponent } from './components/users.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UsersComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    UsersRoutingModule
   ]
 })
 export class UsersModule { }
